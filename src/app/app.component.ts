42438e354d323b970c3f320d436643b3d06c8cfd
Edwin Harly <edwinharly@live.com>
2017-05-17 13:03:00 +0700

    integrate auth0 to header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Voty Vote';
-  isLoggedIn: boolean = true;
+  // isLoggedIn: boolean = true;
+
+  constructor(public auth: AuthService) {
+    auth.handleAuthentication();
+  }
 }
