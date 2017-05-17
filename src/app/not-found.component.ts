906e43d1b40389c642ecc47b3f029c2e16474155
Edwin Harly <edwinharly@live.com>
2017-05-17 11:20:29 +0700

    added notfound component

---
+++ src/app/not-found.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-not-found',
+  template: `
+    <h1>404 NOT FOUND</h1>
+  `
+})
+export class NotFoundComponent {
+  
+}
